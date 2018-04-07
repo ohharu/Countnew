0412ddb91cc5e466c786337421dd9ca766495185
ohharu <ohharu.brain@gmail.com>
2018-04-07 14:58:41 +0900

    カウントアプリを作った

--- Countnew/ViewController.swift
+++ Countnew/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number:Int = 0
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,50 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    func changecolor(){
+        if number >= 10{
+            label.textColor = UIColor.red
+        }
+        else if number <= -10{
+            label.textColor = UIColor.blue
+        }
+        else{
+            label.textColor = UIColor.black
+        }
+    }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        changecolor()
+    }
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        if number >= 10{
+            label.textColor = UIColor.red
+        }
+        else if number <= -10{
+            label.textColor = UIColor.blue
+        }
+        else{
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func time(){
+        number = number * number
+        label.text = String(number)
+        changecolor()
+    }
+    @IBAction func devide(){
+        number = number / number
+        label.text = String(number)
+        changecolor()
+    }
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        label.textColor = UIColor.black
+    }
+}//コメント
 
